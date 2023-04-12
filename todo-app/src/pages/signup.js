b804cb39411f71e07aa5396b7300a31554efb768
Rohan Shanbhag <rohanshanbhag2001@gmail.com>
2023-04-12 15:03:53 -0500

    Clerk SignIn

--- todo-app/src/pages/signup.js
+++ todo-app/src/pages/signup.js
@@ -1,8 +1,16 @@
 import { SignUp } from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, SignIn } from "@clerk/clerk-react";
 
 const SignUpPage = () => (
-  <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" 
-   redirectUrl="/todos"/>
+  // <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" 
+  //  redirectUrl="/todos"/>
+  <SignIn redirectUrl="/todos"/>
 );
+
+// function SignInPage() {
+//   return (
+//     <SignIn />
+//   );
+// }
 
 export default SignUpPage;
